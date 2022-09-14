5ec09b9d663a92bda8401e58ad04d3befaa935e9
AntonioEsp <github email address>
2022-09-13 22:24:32 -0500

    added react-router framer-motion router-dom styled components

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -19,7 +19,7 @@ function Popular() {
         
         {Popular.map((recipe) => {
             return(
-                <div>
+                <div key={recipe.id}>
                     <p>{recipe.title}</p>
                 </div>
             );
