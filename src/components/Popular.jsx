9be4d5e16dd0eac29be192724e7f31b6f37942f2
AntonioEsp <github email address>
2022-09-19 17:44:56 -0500

    third commit

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -3,8 +3,7 @@ import styled from "styled-components";
 
 
 function Popular() {
-
-    const [popular,setPopular] = useState([]);
+ const [popular,setPopular] = useState([]);
 
     useEffect (() => {
         getPopular();
@@ -14,20 +13,36 @@ function Popular() {
         const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
 
         const data = await api.json();
-        setPopular(data.recipies)
-    }
-  return <div>
-        
-        {Popular.map((recipe) => {
-            return(
-                <div key={recipe.id}>
-                    <p>{recipe.title}</p>
-                </div>
-            );
-       
-       })}
-    </div>;
+        setPopular(data.recipies);
+    };
   
+    return( 
+        <div>
+        {popular.map((recipe) => {
+            return (
+                <Wrapper>
+                    <h3>Popular Recipies</h3>
+                    {popular.map((recipe) => {
+                        return(
+                            <Card>
+                                <p>{recipe.title}</p>
+                            </Card>
+                        );
+                    })}
+                </Wrapper>
+            );
+        })}
+    </div>
+    )
 }
+
+const Wrapper = styled.div`
+margin: 4rem 0rem; 
+`;
+
+const Card = styled.div`
+min-height: 25rem;
+border-radius: 2rem; 
+`
 
 export default Popular
